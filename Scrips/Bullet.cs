ac73e4c6443c68049ce90e5399238b561f7cc88a
Tuan-monkey <coin.master.lan.02@gmail.com>
2025-04-11 14:55:23 +0700

    Monkey up code

---
+++ Scrips/Bullet.cs
@@ -0,0 +1,67 @@
+﻿using Fusion;
+using UnityEngine;
+
+public class Bullet : NetworkBehaviour
+{
+    public ParticleSystem bulletTrail; // Hiệu ứng đuôi đạn
+    public GameObject explosionEffect; // Hiệu ứng nổ
+
+    [Networked]
+    public Vector3 NetworkPosition { get; set; }
+
+    [Networked]
+    public Quaternion NetworkRotation { get; set; }
+    private void Start()
+    {
+        bulletTrail.Play();
+        explosionEffect.SetActive(false);
+    }
+    public override void Spawned()
+    {
+        // Kiểm tra và chạy hiệu ứng đuôi đạn
+        if (bulletTrail != null)
+        {
+            bulletTrail.Play();
+        }
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (Object.HasStateAuthority)
+        {
+            NetworkPosition = transform.position;
+            NetworkRotation = transform.rotation;
+        }
+        else
+        {
+            transform.position = NetworkPosition;
+            transform.rotation = NetworkRotation;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Kiểm tra tag và kích hoạt hiệu ứng nổ nếu va chạm với Enemy
+        if (other.CompareTag("Enemy"))
+        {
+            // Gọi RPC để kích hoạt hiệu ứng nổ trên tất cả client
+            Rpc_Explode();
+
+            // Hủy viên đạn trên server
+            if (Object.HasStateAuthority)
+            {
+                Runner.Despawn(Object);
+            }
+        }
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    private void Rpc_Explode()
+    {
+        // Tạo hiệu ứng nổ
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ Scrips/FireMove.cs
@@ -0,0 +1,125 @@
+﻿using Fusion;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class FireMove : NetworkBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform[] FirePoints;
+    public NetworkRunner networkRunner;
+    public float FireRate = 0.1f;
+    public float BulletSpeed = 20f;
+    public float BulletLifetime = 5f;
+
+    // Các trường để gán hiệu ứng từ Editor
+    public GameObject[] FireEffects; // Mảng hiệu ứng bắn
+   // public GameObject[] AudioEffects; // Mảng GameObject âm thanh
+
+    private float nextFireTime = 0f;
+    private bool isFiring = false;
+    private void Start()
+    {
+        StopFiringEffects();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextFireTime)
+        {
+            if (networkRunner is not null && networkRunner.LocalPlayer.IsRealPlayer)
+            {
+                if (!isFiring)
+                {
+                    StartFiringEffects();
+                }
+                isFiring = true;
+                FireBullets();
+                nextFireTime = Time.time + 1f / FireRate;
+            }
+        }
+        else
+        {
+            if (isFiring)
+            {
+                StopFiringEffects();
+            }
+            isFiring = false;
+        }
+    }
+
+    private void FireBullets()
+    {
+        for (int i = 0; i < FirePoints.Length; i++)
+        {
+            if (FirePoints[i] != null)
+            {
+                var bullet = networkRunner.Spawn(BulletPrefab, FirePoints[i].position, FirePoints[i].rotation);
+                var bulletDirection = FirePoints[i].forward;
+                bullet.GetComponent<Rigidbody>().AddForce(bulletDirection * BulletSpeed, ForceMode.Impulse);
+
+                StartCoroutine(DestroyBulletAfterLifetime(bullet));
+            }
+        }
+    }
+
+    private IEnumerator DestroyBulletAfterLifetime(NetworkObject bullet)
+    {
+        yield return new WaitForSeconds(BulletLifetime);
+
+        if (bullet != null && networkRunner != null)
+        {
+            networkRunner.Despawn(bullet);
+        }
+    }
+
+    private void StartFiringEffects()
+    {
+        for (int i = 0; i < FirePoints.Length; i++)
+        {
+            if (FirePoints[i] != null)
+            {
+                // Kích hoạt hiệu ứng bắn nếu có
+                if (FireEffects != null && i < FireEffects.Length && FireEffects[i] != null)
+                {
+                    FireEffects[i].SetActive(true);
+                }
+
+                // Phát âm thanh nếu có
+                //if (AudioEffects != null && i < AudioEffects.Length && AudioEffects[i] != null)
+                //{
+                //    AudioSource audioSource = AudioEffects[i].GetComponent<AudioSource>();
+                //    if (audioSource != null)
+                //    {
+                //        audioSource.Play();
+                //    }
+                //}
+            }
+        }
+    }
+
+    private void StopFiringEffects()
+    {
+        for (int i = 0; i < FirePoints.Length; i++)
+        {
+            if (FirePoints[i] != null)
+            {
+                // Dừng hiệu ứng bắn nếu có
+                if (FireEffects != null && i < FireEffects.Length && FireEffects[i] != null)
+                {
+                    FireEffects[i].SetActive(false);
+                }
+
+                // Dừng âm thanh nếu có
+                //if (AudioEffects != null && i < AudioEffects.Length && AudioEffects[i] != null)
+                //{
+                //    AudioSource audioSource = AudioEffects[i].GetComponent<AudioSource>();
+                //    if (audioSource != null)
+                //    {
+                //        audioSource.Stop();
+                //    }
+                //}
+            }
+        }
+    }
+}
