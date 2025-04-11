d18ed66edf3817d409b29b4382a9a5a62e046b7b
Luan-betriet <coin.master.lan.05@gmail.com>
2025-04-11 15:09:45 +0700

    Betriet up code

---
+++ Scrips_BeTriet/ChatSystem.cs
@@ -0,0 +1,35 @@
+﻿using Fusion;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatSystem : NetworkBehaviour
+{
+    public TextMeshProUGUI TextMess;
+    public TMP_InputField InputMess;
+    public GameObject Gui;
+    public override void Spawned()
+    {
+        // chay khi nhan vat spam
+        //var id = Runner.LocalPlayer.PlayerId;
+        //RpcNotify(id);
+        TextMess = GameObject.Find("Text Mess").GetComponent<TextMeshProUGUI>();
+        InputMess = GameObject.Find("Input Mess").GetComponent<TMP_InputField>();
+        Gui = GameObject.Find("Gui");
+        Gui.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(SendMess);
+    }
+    public void SendMess()
+    {
+        var message = InputMess.text;
+        if (string.IsNullOrWhiteSpace(message)) return;
+        var id = Runner.LocalPlayer.PlayerId;
+        var text = $"player {id}: {message}";
+        RpcChat(text);
+        InputMess.text = "";
+    }
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void RpcChat(string msg)
+    {
+        TextMess.text += msg + "\n";
+    }
+}
---
+++ Scrips_BeTriet/SetSelectedPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseCharacter : MonoBehaviour
+{
+    public int selectedPlayerIndex = 0; // Chỉ số nhân vật được chọn
+
+    public void SetSelectedPlayer(int playerIndex)
+    {
+        selectedPlayerIndex = playerIndex;
+        PlayerPrefs.SetInt("SelectedPlayer", selectedPlayerIndex); // Lưu chỉ số nhân vật
+        SceneManager.LoadScene("Scene3");
+    }
+}
